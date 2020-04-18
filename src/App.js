423400a068400db81f336b56ceb4132085e118a4
Wael Heni <wael.heni@digit-u.com>
2020-04-18 15:11:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h>hi, I'm a react App </h>
+      </div>
+    );
+  }
+}
+
+export default App;
